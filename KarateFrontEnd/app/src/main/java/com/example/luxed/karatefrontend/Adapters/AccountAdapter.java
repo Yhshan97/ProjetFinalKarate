95497852a74fd5a131b7d7d6ce7319d46227dfd4
Luxed <devildead13@gmail.com>
2018-11-24 22:53:53 -0500

    Ajout de network_security_config

--- KarateFrontEnd/app/src/main/java/com/example/luxed/karatefrontend/Adapters/AccountAdapter.java
+++ KarateFrontEnd/app/src/main/java/com/example/luxed/karatefrontend/Adapters/AccountAdapter.java
@@ -42,9 +42,9 @@ public class AccountAdapter extends RecyclerView.Adapter<AccountAdapter.MyViewHo
     public void onBindViewHolder(MyViewHolder holder, int position) {
         Account acc = accounts.get(position);
 
-        //String photoData = acc.getImage().substring(acc.getImage().indexOf(',') + 1);
+        String imgData = acc.getImage().substring(acc.getImage().indexOf(',') + 1);
 
-        byte[] decodedString = Base64.decode(acc.getImage().getBytes(), Base64.URL_SAFE);
+        byte[] decodedString = Base64.decode(imgData.getBytes(), Base64.DEFAULT);
         Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
 
         holder.image.setImageBitmap(decodedByte);
--- KarateFrontEnd/app/src/main/java/com/example/luxed/karatefrontend/MainActivity.java
+++ KarateFrontEnd/app/src/main/java/com/example/luxed/karatefrontend/MainActivity.java
@@ -2,12 +2,56 @@ package com.example.luxed.karatefrontend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvErrorMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        tvErrorMessage = findViewById(R.id.tvErrorMessage);
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://10.0.2.2:8080/lstComptes")
+                .build();
+
+        Log.i("MainActivity", request.toString());
+
+        //String clients = "";
+
+        //Response res = client.newCall(request).execute();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                tvErrorMessage.setText("La liste des comptes n'a pas pu être retournée");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                tvErrorMessage.setText(R.string.ok);
+                try {
+                    Log.i("MainActivity", response.body().string());
+                    JSONObject jsonObject = new JSONObject(response.body().string());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
